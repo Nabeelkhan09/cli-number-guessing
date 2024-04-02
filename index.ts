c8daede11246d2abf4087199738e02778ef0af6e
Nabeelkhan09 <nb4388258@gmail.com>
2024-04-03 01:21:59 +0500

    My First Commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-6: ",
+    },
+]);
+
+if(answer.userGuessedNumber === randomNumber ){
+    console.log("Congratulations! You Guessed Right Number.");
+}
+else {
+    console.log("You Guessed Wrong Number");
+}
